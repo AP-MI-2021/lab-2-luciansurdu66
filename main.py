0c91334fbefa6b8ee9c8b2864f815f4cb76e53df
luciansurdu66 <58331045+luciansurdu66@users.noreply.github.com>
2021-10-05 12:44:22 +0300

    Lab2 done!

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def Eratostene(n):
         while i*j <= primes[-1]:
             if i*j in primes:
                 primes.remove(i*j)
-            j+=1
+            j=j+1
         return primes
 def odd_primes(N):
     '''
@@ -23,6 +23,11 @@ def odd_primes(N):
     return(oddprimes)
 
 def get_goldbach(n):
+    '''
+    verifica conjectura lui Goldbach
+    :param n: nr. intreg
+    :return:cel mai mic si cel mai mare numar prim a caror suma este egala cu n
+    '''
     x,y = 0, 0
     rezultat = 0
     if n%2 == 0:
@@ -36,9 +41,36 @@ def get_goldbach(n):
                     rezultat = x+y
                     if rezultat == n: break
     return x,y
-def test_get_goldbach()
-
-if __name__ == "__main__":
-    n =int(input("Dati un numar"))
-    p1,p2 = get_goldbach(n)
-    print(p1,p2)
+def get_newton_sqrt(n,steps):
+    """
+    Calculeaza rădăcina pătrată
+    :param n: nr real
+    :param steps: nr intreg
+    :return: rădăcina numărului
+    """
+    x= float(n)
+    for i in range(steps):
+        n = 0.5 * (n + x / n)
+    return n
+def test_get_newton_sqrt():
+    assert get_newton_sqrt(81,8) == 9.0
+    assert get_newton_sqrt(5,6) == 2.23606797749979
+shouldRun = True
+while shouldRun:
+    print("1. Determină numerele prime p1 și p2 astfel astfel încât n=p1+p2, p1 minim și p2 maxim")
+    print("2. Determinati daca un nr. este prim")
+    print("x. Iesire")
+    optiune = input("Dati opțiunea: ")
+    if optiune == "1":
+        n= int(input('Dați un număr par >2 : '))
+        p1, p2 = get_goldbach(n)
+        print(p1,p2)
+    elif optiune == "2":
+        test_get_newton_sqrt()
+        x = int(input("Dați un nr."))
+        steps = int(input("Dați un număr de pași: "))
+        print("Rădăcina numărului este: ",get_newton_sqrt(x,steps))
+    elif optiune == "x":
+        shouldRun = False
+    else:
+        print("Opțiune greșită! Reincercați!")
